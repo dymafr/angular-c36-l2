769b441e0dbdf6c4a94721c468335f37dc6cab5a
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-08 19:29:18 +0100

    5 files modified, 2 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  {
-  public form: FormGroup;
+export class AppComponent {
+  public form = this.fb.group({
+    name: [''],
+  });
 
   constructor(public fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.form = this.fb.group({
-      name: ['']
-    });
-  }
-  
 }
-
